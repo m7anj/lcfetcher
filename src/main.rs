f9c99a0cb62122a5ab248dffee393e6bee1e2a21
m7anj <45573549+mxnjv@users.noreply.github.com>
2026-02-15 22:05:23 +0000

    implementation(fighting with rust_analyser): - Chapter 12 of the Rust Book - An I/O Project: Building a Command Line Program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // > lcfetch <number> <location> <language>
+    let args: Vec<String> = env::args().collect();
+
+    let _num: &String = &args[1];
+    let file_path: &String = &args[2];
+    let lang: &String = &args[3];
+
+    
 }
