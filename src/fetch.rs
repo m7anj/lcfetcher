17258d856bb0bd03eed4ce31073bdf84d4e2e0f6
m7anj <45573549+mxnjv@users.noreply.github.com>
2026-02-17 21:09:16 +0000

    implementation(fetch.rs): - initialized a barebones API function called 'api()' passing in a &String - referenced it into the function

---
+++ src/fetch.rs
@@ -0,0 +1,4 @@
+pub fn api(num: &String) {
+    println!("Fetching Problem {0} from API.", num);
+    return;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
+mod fetch;
 
 fn main() {
     // > lcfetch <number> <location> <language>
     let args: Vec<String> = env::args().collect();
 
-    let _num: &String = &args[1];
+    let num: &String = &args[1];
     let file_path: &String = &args[2];
     let lang: &String = &args[3];
 
-    
+    println!(
+        "LeetCode {0}, saved in {1}, file type {2}.",
+        num, file_path, lang
+    );
+
+    fetch::api(num);
 }
